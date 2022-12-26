87b77cf0c9ad30bae49f7c071c1851dab3c412c0
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2022-12-26 11:14:45 +0100

    continue v3

--- trading_bot_v3.py
+++ trading_bot_v3.py
@@ -13,7 +13,7 @@ def get_full_book(df,i):
     ask_price = []
     ask_qty = []
 
-    for i in range(20):
+    for i in range(1,20):
         bid_price.append(row['bid_price'+str(i)].values[0])
         bid_qty.append(row['bid_qty'+str(i)].values[0])
         ask_price.append(row['ask_price'+str(i)].values[0])
@@ -21,7 +21,7 @@ def get_full_book(df,i):
     return bid_price,bid_qty,ask_price,ask_qty
 
 
-def find_position_max(btc_usdt_bid_price,btc_usdt_bid_qty,eth_btc_bid_price,eth_btc_bid_qty,eth_usdt_ask_price,eth_usdt_ask_qty,cash,fee3):
+def find_position_max1(btc_usdt_bid_price,btc_usdt_bid_qty,eth_btc_bid_price,eth_btc_bid_qty,eth_usdt_ask_price,eth_usdt_ask_qty,cash,fee3,eth_btc_full_book_bid_price,eth_btc_full_book_bid_qty,eth_usdt_full_book_ask_price,eth_usdt_full_book_ask_qty,btc_usdt_full_book_bid_price,btc_usdt_full_book_bid_qty):
     #return the biggest cash position we can take on a trade
     # we have 3 trades cash-> btc, btc->eth, eth-> cash
 
@@ -50,13 +50,18 @@ def find_position_max(btc_usdt_bid_price,btc_usdt_bid_qty,eth_btc_bid_price,eth_
     position_max = eth_ask_qty*eth_usdt_ask_price
 
     #variation of the position_max
-    position_max_var *= btc_usdt_bid*eth_btc_bid/eth_usdt_ask*fee3
+    cash -= position_max
 
+    position_max *= btc_usdt_bid*eth_btc_bid/eth_usdt_ask*fee3
+    cash += position_max
     #to update the new bids/asks qty, we need to backpropagate the max postion
     eth_usdt_ask_qty -= max_eth
     if eth_usdt_ask_qty == 0:
         #update price from full book
         #update qty from full book
+        eth_usdt_ask_qty = eth_usdt_full_book_ask_qty.pop(0)
+        eth_usdt_ask_price = eth_usdt_full_book_ask_price.pop(0)
+
     eth_qty = max_eth
     max_btc = eth_qty * max_btc_bid / eth_btc_bid_qty
 
@@ -64,16 +69,78 @@ def find_position_max(btc_usdt_bid_price,btc_usdt_bid_qty,eth_btc_bid_price,eth_
 
     if eth_btc_bid_qty == 0:
         #update price and qty
+        eth_btc_bid_qty = eth_btc_full_book_bid_qty.pop(0)
+        eth_btc_bid_price = eth_btc_full_book_bid_price.pop(0)
+
     btc_qty = max_btc
     max_usdt = max_usdt_bid * btc_qty / btc_usdt_bid_qty
     btc_usdt_bid_qty -= max_usdt / btc_usdt_bid_price
 
     if btc_usdt_bid_qty == 0:
         #update price and qty
-    return position_max
+        btc_usdt_bid_qty = btc_usdt_full_book_bid_qty.pop(0)
+        btc_usdt_bid_price = btc_usdt_full_book_bid_price.pop(0)
+
+    return cash,btc_usdt_bid_qty,btc_usdt_bid_price,btc_usdt_full_book_bid_qty,btc_usdt_full_book_bid_price,eth_btc_bid_qty,eth_btc_bid_price,eth_btc_full_book_bid_qty,eth_btc_full_book_bid_price,eth_usdt_ask_qty,eth_usdt_ask_price,eth_usdt_full_book_ask_qty,eth_usdt_full_book_ask_price
+
+def find_position_max2(btc_usdt_ask_price,btc_usdt_ask_qty,eth_btc_ask_price,eth_btc_ask_qty,eth_usdt_bid_price,eth_usdt_bid_qty,cash,fee3,eth_btc_full_book_ask_price,eth_btc_full_book_ask_qty,eth_usdt_full_book_bid_price,eth_usdt_full_book_bid_qty,btc_usdt_full_book_ask_price,btc_usdt_full_book_ask_qty):
+    # step 1 : compare cash vs usdt_qty in btc_usdt
+
+    #maximum amount of usdt on the bid side for btc_usdt
+    max_usdt_bid = eth_usdt_bid_price*eth_usdt_bid_qty
+    #maximum amount of usdt we can buy
+    max_usdt = min(cash,max_usdt_bid)
+    #maximum amount of btc that can be bought with this amount of usdt
+    eth_qty = eth_usdt_bid_qty * max_usdt / max_usdt_bid
 
 
+    # step 2 :  compare btc_qty vs btc qty in eth_btc
+    max_eth_ask = eth_btc_ask_qty
+    max_eth = min(eth_qty,max_eth_ask)
+    btc_qty = eth_btc_ask_price * max_btc / max_btc_ask
 
+
+    # step 3: compare eth_qty with est qty in eth_usdt
+    max_btc_ask = btc_usdt_ask_qty * btc_usdt_ask_price
+    max_btc = min(btc_qty,max_btc_ask)
+    btc_ask_qty = btc_usdt_ask_qty * max_btc / max_btc_ask
+####
+    #step 4 : convert in usdt (we don't need to compare with cash)
+    position_max = eth_ask_qty*eth_usdt_ask_price
+
+    #variation of the position_max
+    cash -= position_max
+
+    position_max *= btc_usdt_bid*eth_btc_bid/eth_usdt_ask*fee3
+    cash += position_max
+    #to update the new bids/asks qty, we need to backpropagate the max postion
+    eth_usdt_ask_qty -= max_eth
+    if eth_usdt_ask_qty == 0:
+        #update price from full book
+        #update qty from full book
+        eth_usdt_ask_qty = eth_usdt_full_book_ask_qty.pop(0)
+        eth_usdt_ask_price = eth_usdt_full_book_ask_price.pop(0)
+
+    eth_qty = max_eth
+    max_btc = eth_qty * max_btc_bid / eth_btc_bid_qty
+
+    eth_btc_bid_qty -= max_btc / eth_btc_bid_price
+
+    if eth_btc_bid_qty == 0:
+        #update price and qty
+        eth_btc_bid_qty = eth_btc_full_book_bid_qty.pop(0)
+        eth_btc_bid_price = eth_btc_full_book_bid_price.pop(0)
+
+    btc_qty = max_btc
+    max_usdt = max_usdt_bid * btc_qty / btc_usdt_bid_qty
+    btc_usdt_bid_qty -= max_usdt / btc_usdt_bid_price
+
+    if btc_usdt_bid_qty == 0:
+        #update price and qty
+        btc_usdt_bid_qty = btc_usdt_full_book_bid_qty.pop(0)
+        btc_usdt_bid_price = btc_usdt_full_book_bid_price.pop(0)
+
+    return
 # Loading the data
 df_btc_usdt = pd.read_csv('btcusdt_socket_book_async.csv').astype('float32')
 df_eth_usdt = pd.read_csv('ethusdt_socket_book_async.csv').astype('float32')
@@ -139,58 +206,48 @@ for i in range(df.shape[0]):
     # update asks and bids
     if df['book'][i]=='shallow_book':
         if df['market'][i] == 'BTCUSDT' :
-            btc_usdt_bid = df['bid_price'][i]
-            btc_usdt_ask = df['ask_price'][i]
+            btc_usdt_bid_price = df['bid_price'][i]
+            btc_usdt_ask_price = df['ask_price'][i]
             btc_usdt_bid_qty = df['bid_qty'][i]
             btc_usdt_ask_qty = df['ask_qty'][i]
         elif df['market'][i] == 'ETHUSDT':
-            eth_usdt_bid = df['bid_price'][i]
-            eth_usdt_ask = df['ask_price'][i]
+            eth_usdt_bid_price = df['bid_price'][i]
+            eth_usdt_ask_price = df['ask_price'][i]
             eth_usdt_bid_qty = df['bid_qty'][i]
             eth_usdt_ask_qty = df['ask_qty'][i]
         else:
-            eth_btc_bid = df['bid_price'][i]
-            eth_btc_ask = df['ask_price'][i]
+            eth_btc_bid_price = df['bid_price'][i]
+            eth_btc_ask_price = df['ask_price'][i]
             eth_btc_bid_qty = df['bid_qty'][i]
             eth_btc_ask_qty = df['ask_qty'][i]
     else:
         # update the full book
         if df['market'][i] == 'BTCUSDT' :
-            btc_usdt_full_book_bid_price,btc_usdt_full_book_bid_qty,btc_usdt_full_book_ask_price,btc_usdt_full_book_bid_qty = get_full_book(df,i)
+            btc_usdt_full_book_bid_price,btc_usdt_full_book_bid_qty,btc_usdt_full_book_ask_price,btc_usdt_full_book_ask_qty = get_full_book(df,i)
         elif df['market'][i] == 'ETHUSDT':
-            eth_usdt_full_book_bid_price,eth_usdt_full_book_bid_qty,eth_usdt_full_book_ask_price,eth_usdt_full_book_bid_qty = get_full_book(df,i)
+            eth_usdt_full_book_bid_price,eth_usdt_full_book_bid_qty,eth_usdt_full_book_ask_price,eth_usdt_full_book_ask_qty = get_full_book(df,i)
         else:
-            eth_btc_full_book_bid_price,eth_btc_full_book_bid_qty,eth_btc_full_book_ask_price,eth_btc_full_book_bid_qty = get_full_book(df,i)
+            eth_btc_full_book_bid_price,eth_btc_full_book_bid_qty,eth_btc_full_book_ask_price,eth_btc_full_book_ask_qty = get_full_book(df,i)
     # if all market is found once
-    if all([btc_usdt_bid,eth_usdt_bid,eth_btc_bid]):
+    if all([btc_usdt_bid_price,eth_usdt_bid_price,eth_btc_bid_price]):
 
 
         # if we can make a profit with buy btc_usdt, buy eth_btc and sell eth_usdt
         # for the v3, we change the if by a while and we will update, bids/asks as they get empty when we buy
-        while btc_usdt_bid*fee3>eth_usdt_ask/eth_btc_bid:
+        while btc_usdt_bid_price*fee3>eth_usdt_ask/eth_btc_bid:
 
 
             # looking for the biggest possible position with the current order book
-            position_max = find_position_max(btc_usdt_bid,btc_usdt_bid_qty,eth_btc_bid,eth_btc_bid_qty,eth_usdt_ask,eth_usdt_ask_qty,cash)#min(eth_btc_bid_qty*eth_btc_bid*btc_usdt_bid,btc_usdt_bid_qty*btc_usdt_bid,eth_usdt_ask_qty*eth_usdt_ask,eth_btc_bid_qty*eth_usdt_ask,cash)
-
-            # position_max is the biggest cash position we can take from this move,
-            # step 1 : We withdraw it from our cash reserve
-            cash-=position_max
-            # step 2 : We multiply by the gain we can make on this position
-            position_max *= btc_usdt_bid*eth_btc_bid/eth_usdt_ask*fee3
-            # step 3 : we add it back to the cash
-            cash+=position_max
+            cash,btc_usdt_bid_qty,btc_usdt_bid_price,btc_usdt_full_book_bid_qty,btc_usdt_full_book_bid_price,eth_btc_bid_qty,eth_btc_bid_price,eth_btc_full_book_bid_qty,eth_btc_full_book_bid_price,eth_usdt_ask_qty,eth_usdt_ask_price,eth_usdt_full_book_ask_qty,eth_usdt_full_book_ask_price = find_position_max1(btc_usdt_bid_price,btc_usdt_bid_qty,eth_btc_bid_price,eth_btc_bid_qty,eth_usdt_ask_price,eth_usdt_ask_qty,cash,fee3,eth_btc_full_book_bid_price,eth_btc_full_book_bid_qty,eth_usdt_full_book_ask_price,eth_usdt_full_book_ask_qty,btc_usdt_full_book_bid_price,btc_usdt_full_book_bid_qty)#min(eth_btc_bid_qty*eth_btc_bid*btc_usdt_bid,btc_usdt_bid_qty*btc_usdt_bid,eth_usdt_ask_qty*eth_usdt_ask,eth_btc_bid_qty*eth_usdt_ask,cash)
 
             #keep track of when an arbitrage is possible
             arb_possible.append(i)
 
         # if we can make a profit with sell btc_usdt, sell eth_btc and buy eth_usdt
-        elif btc_usdt_ask<eth_usdt_bid/eth_btc_ask*fee3:
+        while btc_usdt_ask_price<eth_usdt_bid_price/eth_btc_ask_price*fee3:
             # looking for the biggest possible position with the current order book
-            position_max = min(eth_btc_ask_qty*eth_btc_ask*btc_usdt_ask,btc_usdt_ask_qty*btc_usdt_ask,eth_usdt_bid_qty*eth_usdt_bid,eth_btc_ask_qty*eth_usdt_bid,cash)
-            cash-=position_max
-            position_max *= eth_usdt_bid/(btc_usdt_ask*eth_btc_ask)*fee3
-            cash+=position_max
+            cash,btc_usdt_ask_qty,btc_usdt_ask_price,btc_usdt_full_book_ask_qty,btc_usdt_full_book_ask_price,eth_btc_ask_qty,eth_btc_ask_price,eth_btc_full_book_ask_qty,eth_btc_full_book_ask_price,eth_usdt_bid_qty,eth_usdt_bid_price,eth_usdt_full_book_bid_qty,eth_usdt_full_book_bid_price = find_position_max2(btc_usdt_ask_price,btc_usdt_ask_qty,eth_btc_ask_price,eth_btc_ask_qty,eth_usdt_bid_price,eth_usdt_bid_qty,cash,fee3,eth_btc_full_book_ask_price,eth_btc_full_book_ask_qty,eth_usdt_full_book_bid_price,eth_usdt_full_book_bid_qty,btc_usdt_full_book_ask_price,btc_usdt_full_book_ask_qty)#min(eth_btc_bid_qty*eth_btc_bid*btc_usdt_bid,btc_usdt_bid_qty*btc_usdt_bid,eth_usdt_ask_qty*eth_usdt_ask,eth_btc_bid_qty*eth_usdt_ask,cash)
+
             arb_possible.append(i)
         cash_time.append(cash)
 
